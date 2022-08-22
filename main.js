c5ba5c2dc83ef8c8675a5c889cc637f70f57f7b4
Milos Savic <msavic248@yahoo.com>
2022-08-22 15:00:38 +0200

    Fixing image not displaying

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import './style.css'
 document.querySelector('#app').innerHTML = `
   <div class="flex-child">
     <div class="card">
-      <img class="card-image" src="/public/images/image-qr-code.png" alt="QR code">
+      <img class="card-image" src="/images/image-qr-code.png" alt="QR code">
       <h1 class="card-title">Improve your front-end<br>skills by building projects</h1>
       <p class="card-paragraph">Scan the QR code to visit Frontend<br>Mentor and take your coding skills to the next level</p>
     </div>
